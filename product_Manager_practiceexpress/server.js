3aaa63154f468e5fceb362451b9a06547c13b3fa
Vishal <vishaljankar2534@gmail.com>
2026-05-12 08:59:06 +0530

    fixed submodule issue

---
+++ product_Manager_practiceexpress/server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+const port = 3000
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`)
+})
+
+app.get('/', (req, res) => {
+  res.send("Welcome to Product Inventory!")
+})
+
+let products = [
+  { id: 1, name: 'Camera', price: 10000, stock: 10 },
+  { id: 2, name: 'Laptop', price: 55000, stock: 5  },
+]
+app.get('/products', (req, res) => {
+  res.json({products: products})
+})
+app.get('/products/:id',(req,res)=>{
+  const id=parseInt(req.params.id)
+  const product=products.find(p=>p.id==id)
+  if(!product){
+    return res.status(404).json({error:`Product with id ${id} not found`})
+  }
+  res.json({product: product})
+})
+  
