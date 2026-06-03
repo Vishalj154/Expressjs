aed3697b13a5539521a319385c89d24f2dbb5050
Vishal <vishaljankar2534@gmail.com>
2026-06-03 15:17:06 +0530

    cors logic added

--- product_Manager_practiceexpress/server.js
+++ product_Manager_practiceexpress/server.js
@@ -1,6 +1,8 @@
-const express = require('express')
+const express = require('express')                
 const path = require('path') 
 const app = express()
+const cors = require('cors')    
+app.use(cors()) 
 
 // ── Middleware ──
 app.set('view engine', 'ejs')
