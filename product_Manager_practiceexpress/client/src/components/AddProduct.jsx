09dedd71bad059c41600e3cf34012d86fef4d67d
Vishal <vishaljankar2534@gmail.com>
2026-06-03 15:48:42 +0530

    in react addproduct logic added

--- product_Manager_practiceexpress/client/src/components/AddProduct.jsx
+++ product_Manager_practiceexpress/client/src/components/AddProduct.jsx
@@ -5,19 +5,29 @@ const AddProduct = () => {
     const [name, setName] = useState('');
     const [price, setPrice] = useState('');
     const [stock, setStock] = useState('');
+    const handlesubmit = (e) => {
+        e.preventDefault();
+        const product = { name, price, stock }
+        fetch('https://localhost:300/products', {
+            method: "Post",
+            headers: {
+                'Content-Type': 'application/json'  
+            },
+            body: JSON.stringify(product)
+        })
 
-    return (
-        <>
-            <div>AddProduct</div>
-            <form>
-                <input type="text" value={name} onChange={e => setName(e.target.value)} />
-                <input type="number" value={price} onChange={e => setPrice(e.target.value)} />
-                <input type="number" value={stock} onChange={e => setStock(e.target.value)} />
-                <button type="submit">Add Product</button>
-            </form>
-        </>
+        return (
+            <>
+                <div>AddProduct</div>
+                <form onSubmit={handlesubmit}>
+                    <input type="text" value={name} onChange={e => setName(e.target.value)} />
+                    <input type="number" value={price} onChange={e => setPrice(e.target.value)} />
+                    <input type="number" value={stock} onChange={e => setStock(e.target.value)} />
+                    <button type="submit" onclick={handlesubmit}>Add Product</button>
+                </form>
+            </>
 
-    )
-}
+        )
+    }
 
-export default AddProduct
+    export default AddProduct
